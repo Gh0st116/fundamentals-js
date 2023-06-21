5fafaecebd8c2e082c0b49a9bd8b35b510517113
Juan Pablo <pablo.jp116@gmail.com>
2023-06-21 16:39:38 -0300

    Class three example

---
+++ console.js
@@ -0,0 +1,10 @@
+// Log -> registro
+const minhaVar = true;
+
+console.log(1234);
+console.log("Eu sou um texto");
+console.log(minhaVar);
+
+
+// Tratamento de erro
+console.error(new Error("Deu erro!"));
